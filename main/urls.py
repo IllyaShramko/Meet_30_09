f8bbacd8f16177e463be15ce1d3167394e08e73e
Illya Shramko <123illya123123r@gmail.com>
2024-09-30 20:50:36 +0300

    merge Egor and David brunches

---
+++ main/urls.py
@@ -0,0 +1,25 @@
+import flask
+from .settings import main_project
+import home, tour, user
+
+home.home_page.add_url_rule(
+    rule= "/",
+    view_func= home.render_home
+)
+main_project.register_blueprint(blueprint= home.home_page)
+
+tour.tour_app.add_url_rule(
+    rule= "/tour/",
+    view_func= tour.render_tour
+)
+main_project.register_blueprint(blueprint= tour.tour_app)
+
+user.user_app.add_url_rule(
+    rule= "/registration/",
+    view_func= user.render_registration
+)
+user.user_app.add_url_rule(
+    rule= "/login/",
+    view_func= user.render_login
+)
+main_project.register_blueprint(blueprint= user.user_app)
