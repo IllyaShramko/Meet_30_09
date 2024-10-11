295bd27dc17e7991a16fb2788b69d378af60231f
Illya Shramko <123illya123123r@gmail.com>
2024-10-11 21:23:49 +0300

    Added not working registration (in process)

--- main/settings.py
+++ main/settings.py
@@ -1,6 +1,11 @@
-import flask
+import flask, flask_sqlalchemy, flask_migrate
 
 main_project = flask.Flask(
     import_name= "main",
     template_folder= "templates"
 )
+main_project.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///data.db"
+
+DATABASE = flask_sqlalchemy.SQLAlchemy(app= main_project)
+
+migrate = flask_migrate.Migrate(app= main_project, db= DATABASE)
--- user/views.py
+++ user/views.py
@@ -1,6 +1,25 @@
 import flask
+from .models import User
+from main.settings import DATABASE
+import flask_login
 
+def render_registration():
+    confirmed = False
+    if flask.request.method == 'POST':
+        
+
+        user = User(
+            login = flask.request.form['login'],
+            password = flask.request.form['password'],
+        )
+        try:
+            DATABASE.session.add(user)
+            DATABASE.session.commit()
+            return flask.redirect("/login/")
+        except:
+            return "Не вдалося створити користувача"  
+    print(confirmed)
+    return flask.render_template(
+        template_name_or_list='register.html')
 def render_login():
     return flask.render_template("login.html")
-def render_registration():
-    return flask.render_template("registration.html")
