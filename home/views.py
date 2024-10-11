28ea89eda2257ca88c847928d29a0d92f368ce74
David Petrenko <121516101+Davidptn@users.noreply.github.com>
2024-10-11 21:27:39 +0300

    add not working send on email(in proces)

--- home/views.py
+++ home/views.py
@@ -1,4 +1,20 @@
 import flask
+from main.settings import DATABASE
+from main.flask_config import mail
+import flask_mail
 
 def render_home():
+    try:
+        text_message = f'Клієнт {flask.request.form["client_name"]} залишив(-ла) відгук:\n\n{flask.request.form["message"]}:\n\n{flask.request.form["client_email"]}'
+        text_message1 = flask_mail.Message(
+            subject = "Відгук від клієнта",
+            body = text_message,
+            recipients = ["123illya123123r@gmail.com"],
+            sender = flask.request.form["client_email"]
+        )
+        mail.send(text_message1)
+    except:
+        print("")
     return flask.render_template("home.html")
+    
+        
---
+++ main/flask_config.py
@@ -0,0 +1,10 @@
+import flask_mail
+from .settings import main_project
+
+main_project.config["MAIL_SERVER"] = "smtp.gmail.com"
+main_project.config["PORT"] = 587
+main_project.config["MAIL_USE_TLS"] = True
+main_project.config["MAIL_USERNAME"] = "123illya123123r@gmail.com"
+main_project.config["MAIL_PASSWORD"] = "jdcy afuy ozyl wlme"
+
+mail = flask_mail.Mail(main_project)
