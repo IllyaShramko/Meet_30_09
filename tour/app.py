07609a4acb58c6a2067ff0fddcd6324afd4de1c3
Illya Shramko <123illya123123r@gmail.com>
2024-11-16 20:58:46 +0200

    Added styles for home page with auth (by David); added styles for tour & tours page (by Egor)

--- tour/app.py
+++ tour/app.py
@@ -2,5 +2,7 @@ import flask
 tour_app = flask.Blueprint(
     name= "tour",
     import_name= "tour",
-    template_folder= "templates"
+    template_folder= "templates",
+    static_url_path = "/tour/",
+    static_folder = "static"
 )
